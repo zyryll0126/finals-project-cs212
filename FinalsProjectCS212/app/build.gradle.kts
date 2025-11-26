2a7c975b29c8bd670466b9dbea31ae14c49d3072
geoff Dulnuan <jipjipkisew@gmail.com>
2025-11-26 14:20:17 +0800

    integrated firebase

--- FinalsProjectCS212/app/build.gradle.kts
+++ FinalsProjectCS212/app/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     alias(libs.plugins.android.application)
+    id("com.google.gms.google-services")
 }
 
 android {
@@ -41,4 +42,6 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
+    implementation(platform("com.google.firebase:firebase-bom:34.6.0"))
+    implementation("com.google.firebase:firebase-analytics")
 }
